4f4efc8d13a63d39da94b55c879c525a38e50870
Houssam-Benayache <100476004+bingo132@users.noreply.github.com>
2023-01-03 22:59:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const cours = require('./cours');
+const users = require('./users');
 require("./config");
 const app = express();
 
 app.use(express.json());
 
 app.get("/search/:key",async (req,resp)=>{
-    let data = await cours.find(
+    let courses = await cours.find(
         {
             "$or":[
                 {name:{$regex:req.params.key}},
-                {module:{$regex:req.params.key}}
+                {module:{$regex:req.params.key}},
             ]
         }
     )
-    resp.send(data);
+    let user = await users.find(
+        {
+            username:{$regex:req.params.key}
+               }
+    )
+    resp.status(200).send("success courses search",(courses))
+    resp.status(200).send("success users search",(user))
+   
 
 })
 
